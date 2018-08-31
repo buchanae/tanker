e4a20a1e9bba1d5da6c5eb7170e9501b0c68523c
Alex Buchanan <buchanae@ohsu.edu>
2018-08-30 18:00:47 -0700

    global tanker config

--- main.go
+++ main.go
@@ -27,48 +27,67 @@ type Tanker struct {
 }
 
 func (t *Tanker) Close() error {
-  t.LogFile.Close()
+	if t.LogFile != nil {
+		t.LogFile.Close()
+	}
   return nil
 }
 
 func NewTanker() (*Tanker, error) {
   repodir, err := findRepoRoot()
   if err != nil {
-    return nil, fmt.Errorf("finding git repo root: %s", err)
+		if strings.HasPrefix(err.Error(), "fatal: not a git repository") {
+			// It's ok if we're not in a git repo, we'll just skip some steps.
+			err = nil
+		} else {
+			return nil, fmt.Errorf("finding git repo root: %s", err)
+		}
   }
 
   tanker := &Tanker{}
-  tanker.Paths.Repo = repodir
-  tanker.Paths.Git = filepath.Join(tanker.Paths.Repo, ".git")
-  tanker.Paths.Tanker = filepath.Join(tanker.Paths.Git, "tanker")
-  tanker.Paths.Logs = filepath.Join(tanker.Paths.Tanker, "logs")
-  tanker.Paths.Data = filepath.Join(tanker.Paths.Tanker, "data")
-  tanker.Paths.Config = filepath.Join(tanker.Paths.Tanker, "config.yml")
 
-  // Initialize logging to a file.
-  err = storage.EnsurePath(tanker.Paths.Logs)
-	if err != nil {
-    return nil, fmt.Errorf("initializing logging file: %s", err)
-	}
-	logfh, err := os.OpenFile(tanker.Paths.Logs, os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-    return nil, fmt.Errorf("initializing logging file: %s", err)
-	}
-  tanker.LogFile = logfh
-	log.SetOutput(logfh)
+	if repodir != "" {
+		tanker.Paths.Repo = repodir
+		tanker.Paths.Git = filepath.Join(tanker.Paths.Repo, ".git")
+		tanker.Paths.Tanker = filepath.Join(tanker.Paths.Git, "tanker")
+		tanker.Paths.Logs = filepath.Join(tanker.Paths.Tanker, "logs")
+		tanker.Paths.Data = filepath.Join(tanker.Paths.Tanker, "data")
+		tanker.Paths.Config = filepath.Join(tanker.Paths.Tanker, "config.yml")
 
-  // Initialize a directory for writing tanker data during download.
-	err = storage.EnsureDir(tanker.Paths.Data)
-	if err != nil {
-    return nil, fmt.Errorf("initializing data directory: %s", err)
-	}
+		// Initialize logging to a file.
+		err = storage.EnsurePath(tanker.Paths.Logs)
+		if err != nil {
+			return nil, fmt.Errorf("initializing logging file: %s", err)
+		}
+		logfh, err := os.OpenFile(tanker.Paths.Logs, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, fmt.Errorf("opening logging file: %s", err)
+		}
+		tanker.LogFile = logfh
+		log.SetOutput(logfh)
 
-  // Load a tanker config file.
-  tanker.Config = DefaultConfig()
-  err = ParseConfigFile(tanker.Paths.Config, &tanker.Config)
-  if err != nil {
-    return nil, fmt.Errorf("parsing config: %s", err)
-  }
+		// Initialize a directory for writing tanker data during download.
+		err = storage.EnsureDir(tanker.Paths.Data)
+		if err != nil {
+			return nil, fmt.Errorf("initializing data directory: %s", err)
+		}
+
+		tanker.Config = DefaultConfig()
+
+		// Ensure the config file exists.
+		if _, err := os.Open(tanker.Paths.Config); os.IsNotExist(err) {
+			err := WriteConfigFile(tanker.Config, tanker.Paths.Config)
+			if err != nil {
+				return nil, fmt.Errorf("writing default config file: %s", err)
+			}
+		}
+
+		// Load a tanker config file.
+		err = ParseConfigFile(tanker.Paths.Config, &tanker.Config)
+		if err != nil {
+			return nil, fmt.Errorf("parsing config: %s", err)
+		}
+	}
 
   return tanker, nil
 }
@@ -83,6 +102,38 @@ func main() {
     Use: "init <base url>",
     Args: cobra.ExactArgs(1),
     RunE: func(_ *cobra.Command, args []string) error {
+			url := args[0]
+
+			if len(url) == 0 {
+				return fmt.Errorf("empty URL")
+			}
+
+			if url == "swift://" {
+				return fmt.Errorf("invalid URL: a bucket name is required") 
+			}
+
+			if !strings.HasPrefix(url, "swift://") {
+				return fmt.Errorf("invalid URL: tanker currently only supports swift://")
+			}
+
+			key := fmt.Sprintf("lfs.%s.standalonetransferagent", url)
+      cmd := exec.Command("git", "config", "--global", key, "tanker")
+      err := cmd.Run()
+      if err != nil {
+        return fmt.Errorf("configuring git-lfs: %s", err)
+      }
+
+      cmd = exec.Command("git", "config", "--global", "lfs.customtransfer.tanker.path", "tanker")
+      err = cmd.Run()
+      if err != nil {
+        return fmt.Errorf("configuring git-lfs: %s", err)
+      }
+
+      cmd = exec.Command("git", "config", "--global", "lfs.customtransfer.tanker.args", "transfer")
+      err = cmd.Run()
+      if err != nil {
+        return fmt.Errorf("configuring git-lfs: %s", err)
+      }
 
       tanker, err := NewTanker()
       if err != nil {
@@ -90,36 +141,22 @@ func main() {
       }
       defer tanker.Close()
 
-      cmd := exec.Command("git", "config", "lfs.url", "tanker")
-      err = cmd.Run()
-      if err != nil {
-        return fmt.Errorf("configuring git-lfs: %s", err)
-      }
+			// If we're in a git repo, set up the repo.
+			if tanker.Paths.Repo != "" {
 
-      cmd = exec.Command("git", "config", "lfs.standalonetransferagent", "tanker")
-      err = cmd.Run()
-      if err != nil {
-        return fmt.Errorf("configuring git-lfs: %s", err)
-      }
+				cmd := exec.Command("git", "config", "-f", ".lfsconfig", "lfs.url", url)
+				err = cmd.Run()
+				if err != nil {
+					return fmt.Errorf("configuring git-lfs: %s", err)
+				}
 
-      cmd = exec.Command("git", "config", "lfs.customtransfer.tanker.path", "tanker")
-      err = cmd.Run()
-      if err != nil {
-        return fmt.Errorf("configuring git-lfs: %s", err)
-      }
-
-      cmd = exec.Command("git", "config", "lfs.customtransfer.tanker.args", "transfer")
-      err = cmd.Run()
-      if err != nil {
-        return fmt.Errorf("configuring git-lfs: %s", err)
-      }
-
-      url := args[0]
-      tanker.Config.BaseURL = url
-      err = WriteConfigFile(tanker.Config, tanker.Paths.Config)
-      if err != nil {
-        return fmt.Errorf("writing config file: %s", err)
-      }
+				// TODO just derive from lfs.url
+				tanker.Config.BaseURL = url
+				err = WriteConfigFile(tanker.Config, tanker.Paths.Config)
+				if err != nil {
+					return fmt.Errorf("writing config file: %s", err)
+				}
+			}
 
       return nil
     },
@@ -181,12 +218,12 @@ func main() {
 // findRepoRoot finds the root of the repo.
 func findRepoRoot() (string, error) {
   cmd := exec.Command("git", "rev-parse", "--show-toplevel")
-  out, err := cmd.Output()
+  out, err := cmd.CombinedOutput()
   if err != nil {
-    return "", fmt.Errorf("finding .git directory: %s", err)
+    return "", fmt.Errorf("%s", out)
   }
   if len(out) == 0 {
-    return "", fmt.Errorf("finding .git directory: empty output")
+    return "", fmt.Errorf("empty output")
   }
   path := string(out)
   path = strings.TrimSpace(path)
